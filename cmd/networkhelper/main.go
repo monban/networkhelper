b3effa671c3cd02c0a6bf7a8f397f16f328d998a
Ian Flynn <himitsu.monban@gmail.com>
2023-10-18 22:57:43 -0400

    ask if DHCP is wanted

--- cmd/networkhelper/main.go
+++ cmd/networkhelper/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"os/exec"
 
 	_ "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
+	"github.com/erikgeiser/promptkit/confirmation"
 )
 
 func main() {
-	fmt.Println("hello, world")
+	c := confirmation.New("Connect DHCP?", confirmation.Yes)
+	dhcp, err := c.RunPrompt()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if dhcp {
+		x("udhcpc")
+	}
+}
+
+func x(cmd string) {
+	if os.Getenv("USER") == "root" {
+		exec.Command(cmd).Run()
+	} else {
+		log.Infof("SIMULATION: %s", cmd)
+	}
 }
